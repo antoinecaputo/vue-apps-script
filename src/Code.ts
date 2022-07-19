a16bfb590f1e863181b8e2bf6a9a93afa414e511
Antoine Caputo <antoine.bamboutech@icloud.com>
2022-07-19 17:39:39 +0200

    Dev first commit

--- src/Code.ts
+++ src/Code.ts
@@ -1,8 +1,10 @@
+const VERSION = "1.0.1";
+
 function doGet(req) {
-  return ContentService.createTextOutput(`Hello from Vue Apps Script Production ${fctVersion()}`);
+  return ContentService.createTextOutput(`Hello from Vue Apps Script Dev ${fctVersion()}`);
 }
 
 
-function fctVersion() : String{
-  return "1.0"
+function fctVersion() : string{
+  return VERSION;
 }
